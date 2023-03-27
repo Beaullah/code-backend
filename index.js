40d58fbf8ef95605dfee507775c1b8e8ea9dc394
Esther Fummey@ <bonita19fummey@gmail.com>
2023-03-27 10:34:48 -0700

    second

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const http = require("http");
- 
-const handleAllRequest = () =>{
-    console.log("hi , i just received a request")
-}
+ const handleAllRequest = function (requestObject, responseObject){
+    console.log("Hi, I just received a request");
+    const url = requestObject.url;
+    if (url == "/"){
+      responseObject.write("<h1>Welcome to the home page </h1>")
+    }
+    else if (url === '/login'){
+      responseObject.write("This is ur response")
+    }
+    else if (url === '/login'){
+      responseObject.write("This is ur response")
+    }
+    else if (url === '/signup'){
+      responseObject.write("Welcome ")
+    }
     
+   
+ }
 const server = http.createServer(handleAllRequest);
 
 server.listen(3000, "127.0.0.1", function (){
